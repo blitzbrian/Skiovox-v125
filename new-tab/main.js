4f9cecd00d4e82e472158052cb06df6390987122
CodingHarryJarry <148587892+CodingHarryJarry@users.noreply.github.com>
2025-03-15 01:24:49 -0400

    Fixed google signin

--- new-tab/main.js
+++ new-tab/main.js
@@ -13,7 +13,7 @@ const NEW_TAB_URL = "chrome://new-tab-page";
 const FILES_URL = "chrome://file-manager";
 const HELP_URL = "https://github.com/bypassiwastaken/skiovox-helper";
 const WEBSTORE_URL = "https://chromewebstore.google.com";
-const ADDSESSION_URL = "https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&flowName=GlifWebSignIn&flowEntry=AddSession";
+const ADDSESSION_URL = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2F&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession";
 
 let [
     help,
